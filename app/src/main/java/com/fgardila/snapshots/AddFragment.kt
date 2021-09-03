9cde40e957c02af2a527fe6875c1d4e742fbe1c5
Fabian Guillermo Ardila Castro <fabianardila@wposs.com>
2021-09-03 14:39:09 -0500

    Se agrega la imagen pero se esta presentando un error.

--- app/src/main/java/com/fgardila/snapshots/AddFragment.kt
+++ app/src/main/java/com/fgardila/snapshots/AddFragment.kt
@@ -1,19 +1,55 @@
 package com.fgardila.snapshots
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fgardila.snapshots.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
 
+    private val RC_GALLERY = 18
+
+    private lateinit var mBindign: FragmentAddBinding
+
+    private var mPhotoSelectedUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        mBindign = FragmentAddBinding.inflate(inflater, container, false)
+        return mBindign.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mBindign.btnPost.setOnClickListener { postSnapshot() }
+
+        mBindign.btnSelect.setOnClickListener { openGallery() }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, RC_GALLERY)
+    }
+
+    private fun postSnapshot() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == RC_GALLERY) {
+                mPhotoSelectedUri = data?.data
+                mBindign.imgPhoto.setImageURI(mPhotoSelectedUri)
+            }
+        }
     }
 }
