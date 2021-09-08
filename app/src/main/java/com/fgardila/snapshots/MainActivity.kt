c1fd9e9152a28e8e1ff1405f3074ffd43974c4a8
Fabian Guillermo Ardila Castro <fabianardila@wposs.com>
2021-09-08 15:33:16 -0500

    Login y Logout usando Auth UI

--- app/src/main/java/com/fgardila/snapshots/MainActivity.kt
+++ app/src/main/java/com/fgardila/snapshots/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.fgardila.snapshots
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.fgardila.snapshots.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN = 21
 
     private lateinit var mBinding: ActivityMainBinding
     private lateinit var mActiveFragment: Fragment
     private lateinit var mFragmentManager: FragmentManager
+    private lateinit var mAuthListener: FirebaseAuth.AuthStateListener
+
+    private var mFirebaseAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
+        setupAuth()
         setupBottomNav()
+    }
+
+    private fun setupAuth() {
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mAuthListener = FirebaseAuth.AuthStateListener {
+            val user = it.currentUser
+            if (user == null) {
+                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                    .setAvailableProviders(
+                        Arrays.asList(
+                            AuthUI.IdpConfig.EmailBuilder().build(),
+                            AuthUI.IdpConfig.GoogleBuilder().build()
+                        )
+                    ).build(), RC_SIGN_IN)
+            }
+        }
     }
 
     private fun setupBottomNav() {
@@ -67,6 +96,29 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 else -> false
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mFirebaseAuth?.addAuthStateListener(mAuthListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mFirebaseAuth?.removeAuthStateListener(mAuthListener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            } else {
+                if (IdpResponse.fromResultIntent(data) == null) {
+                    finish()
+                }
             }
         }
     }
